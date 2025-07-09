2de6680222ab201f384af8704532e26a13a25a15
ArqTras <33489188+ArqTras@users.noreply.github.com>
2025-07-09 22:41:23 +0200

    revert
    revert

--- llarp/constants/version.hpp
+++ llarp/constants/version.hpp
@@ -5,13 +5,13 @@
 
 namespace llarp
 {
-  // Given a full arqnet version of: arqnet-1.2.3-abc these are:
-  extern const std::array< uint16_t, 3 > VERSION;         // [1, 2, 3]
-  extern const std::array< uint64_t, 4 > ROUTER_VERSION;  // [proto, 1, 2, 3]
-  extern const char* const VERSION_STR;                   // "1.2.3"
-  extern const char* const VERSION_TAG;                   // "abc"
-  extern const char* const VERSION_FULL;                  // "arqnet-1.2.3-abc"
+  // Example: arqnet-1.2.3-abc
+  extern const std::array<uint16_t, 3> VERSION;         // [1, 2, 3]
+  extern const std::array<uint64_t, 4> ROUTER_VERSION;  // [proto, 1, 2, 3]
+  extern const char* const VERSION_STR;                 // "1.2.3"
+  extern const char* const VERSION_TAG;                 // "abc"
+  extern const char* const VERSION_FULL;                // "arqnet-1.2.3-abc"
 
-  extern const char* const RELEASE_MOTTO;
-  extern const char* const DEFAULT_NETID;
+  extern const char* const RELEASE_MOTTO;               // "Privacy is a Must"
+  extern const char* const DEFAULT_NETID;               // e.g. "arqmainnet"
 }  // namespace llarp
